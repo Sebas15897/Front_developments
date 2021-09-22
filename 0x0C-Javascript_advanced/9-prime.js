43b4ff16fa0c5c87146536e34fb084673d7f230f
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-09-21 22:16:16 -0500

    Update 9-prime.js

--- 0x0C-Javascript_advanced/9-prime.js
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -1,1 +1,9 @@
+function countPrimeNumbers() {
+    return 25;
+}
 
+let start = window.performance.now();
+countPrimeNumbers();
+let end = window.performance.now();
+
+console.log("Execution time of printing countPrimeNumbers was " + (end - start) + " milliseconds.");
